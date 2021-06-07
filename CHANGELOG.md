d775913cd9dd5546522e9b8e7691feec88120cf4
Cody Cutrer <cody@instructure.com>
2021-06-07 09:44:56 -0600

    add helper to defer and restore constraint checking

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## [Unreleased]
 
+- Add PostgreSQLAdapter#defer_constraints
+
 ## [0.1.0] - 2021-06-02
 
 - Initial release
--- spec/postgresql_adapter_spec.rb
+++ spec/postgresql_adapter_spec.rb
@@ -25,4 +25,19 @@ describe ActiveRecord::ConnectionAdapters::PostgreSQLAdapter do
       expect(connection.executed_statements).to eq ['SET CONSTRAINTS "my_fk1", "my_fk2" DEFERRED']
     end
   end
+
+  describe "#defer_constraints" do
+    around do |example|
+      connection.dont_execute(&example)
+    end
+
+    it "defers and resets" do
+      block_called = false
+      connection.defer_constraints do
+        block_called = true
+      end
+      expect(block_called).to eq true
+      expect(connection.executed_statements).to eq ["SET CONSTRAINTS ALL DEFERRED", "SET CONSTRAINTS ALL IMMEDIATE"]
+    end
+  end
 end
